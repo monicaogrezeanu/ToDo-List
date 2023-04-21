764ab79c2f34cca60152b4ab8b0c45e4fe22740e
Monica <Monica@DESKTOP-6AUK8PL>
2023-04-21 19:47:51 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace Tema5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip3 = new System.Windows.Forms.MenuStrip();
+            this.fiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nouToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deschidereToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ieșireToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.adăugațiSarcinăToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editațiSarcinaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ștergețiSarcinaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajutorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.despreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.menuStrip3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip3
+            // 
+            this.menuStrip3.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip3.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fiToolStripMenuItem,
+            this.editareToolStripMenuItem,
+            this.ajutorToolStripMenuItem});
+            this.menuStrip3.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip3.Name = "menuStrip3";
+            this.menuStrip3.Size = new System.Drawing.Size(1009, 33);
+            this.menuStrip3.TabIndex = 2;
+            this.menuStrip3.Text = "menuStrip3";
+            // 
+            // fiToolStripMenuItem
+            // 
+            this.fiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nouToolStripMenuItem,
+            this.deschidereToolStripMenuItem,
+            this.salvareToolStripMenuItem,
+            this.ieșireToolStripMenuItem});
+            this.fiToolStripMenuItem.Name = "fiToolStripMenuItem";
+            this.fiToolStripMenuItem.Size = new System.Drawing.Size(68, 29);
+            this.fiToolStripMenuItem.Text = "Fișier";
+            // 
+            // nouToolStripMenuItem
+            // 
+            this.nouToolStripMenuItem.Name = "nouToolStripMenuItem";
+            this.nouToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.nouToolStripMenuItem.Text = "Nou";
+            this.nouToolStripMenuItem.Click += new System.EventHandler(this.nouToolStripMenuItem_Click);
+            // 
+            // deschidereToolStripMenuItem
+            // 
+            this.deschidereToolStripMenuItem.Name = "deschidereToolStripMenuItem";
+            this.deschidereToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.deschidereToolStripMenuItem.Text = "Deschidere";
+            this.deschidereToolStripMenuItem.Click += new System.EventHandler(this.deschidereToolStripMenuItem_Click);
+            // 
+            // salvareToolStripMenuItem
+            // 
+            this.salvareToolStripMenuItem.Name = "salvareToolStripMenuItem";
+            this.salvareToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.salvareToolStripMenuItem.Text = "Salvare";
+            this.salvareToolStripMenuItem.Click += new System.EventHandler(this.salvareToolStripMenuItem_Click);
+            // 
+            // ieșireToolStripMenuItem
+            // 
+            this.ieșireToolStripMenuItem.Name = "ieșireToolStripMenuItem";
+            this.ieșireToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.ieșireToolStripMenuItem.Text = "Ieșire";
+            this.ieșireToolStripMenuItem.Click += new System.EventHandler(this.ieșireToolStripMenuItem_Click);
+            // 
+            // editareToolStripMenuItem
+            // 
+            this.editareToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.adăugațiSarcinăToolStripMenuItem,
+            this.editațiSarcinaToolStripMenuItem,
+            this.ștergețiSarcinaToolStripMenuItem});
+            this.editareToolStripMenuItem.Name = "editareToolStripMenuItem";
+            this.editareToolStripMenuItem.Size = new System.Drawing.Size(82, 29);
+            this.editareToolStripMenuItem.Text = "Editare";
+            // 
+            // adăugațiSarcinăToolStripMenuItem
+            // 
+            this.adăugațiSarcinăToolStripMenuItem.Name = "adăugațiSarcinăToolStripMenuItem";
+            this.adăugațiSarcinăToolStripMenuItem.Size = new System.Drawing.Size(245, 34);
+            this.adăugațiSarcinăToolStripMenuItem.Text = "Adăugați sarcină";
+            this.adăugațiSarcinăToolStripMenuItem.Click += new System.EventHandler(this.adăugațiSarcinăToolStripMenuItem_Click);
+            // 
+            // editațiSarcinaToolStripMenuItem
+            // 
+            this.editațiSarcinaToolStripMenuItem.Name = "editațiSarcinaToolStripMenuItem";
+            this.editațiSarcinaToolStripMenuItem.Size = new System.Drawing.Size(245, 34);
+            this.editațiSarcinaToolStripMenuItem.Text = "Editați sarcina";
+            this.editațiSarcinaToolStripMenuItem.Click += new System.EventHandler(this.editațiSarcinaToolStripMenuItem_Click);
+            // 
+            // ștergețiSarcinaToolStripMenuItem
+            // 
+            this.ștergețiSarcinaToolStripMenuItem.Name = "ștergețiSarcinaToolStripMenuItem";
+            this.ștergețiSarcinaToolStripMenuItem.Size = new System.Drawing.Size(245, 34);
+            this.ștergețiSarcinaToolStripMenuItem.Text = "Ștergeți sarcina";
+            this.ștergețiSarcinaToolStripMenuItem.Click += new System.EventHandler(this.ștergețiSarcinaToolStripMenuItem_Click);
+            // 
+            // ajutorToolStripMenuItem
+            // 
+            this.ajutorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.despreToolStripMenuItem});
+            this.ajutorToolStripMenuItem.Name = "ajutorToolStripMenuItem";
+            this.ajutorToolStripMenuItem.Size = new System.Drawing.Size(77, 29);
+            this.ajutorToolStripMenuItem.Text = "Ajutor";
+            // 
+            // despreToolStripMenuItem
+            // 
+            this.despreToolStripMenuItem.Name = "despreToolStripMenuItem";
+            this.despreToolStripMenuItem.Size = new System.Drawing.Size(170, 34);
+            this.despreToolStripMenuItem.Text = "Despre";
+            this.despreToolStripMenuItem.Click += new System.EventHandler(this.despreToolStripMenuItem_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(12, 51);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(406, 504);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(478, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 48);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Sarcină";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(478, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nume";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(474, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Descriere";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(474, 344);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Dată scadență";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(474, 428);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(128, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Nivel prioritate";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(664, 147);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(320, 31);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(664, 223);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(320, 83);
+            this.textBox2.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(565, 505);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Salvare";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(774, 505);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 34);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Ștergere";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(664, 344);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(126, 31);
+            this.dateTimePicker1.TabIndex = 15;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(664, 426);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(67, 31);
+            this.numericUpDown1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 589);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip3.ResumeLayout(false);
+            this.menuStrip3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MenuStrip menuStrip3;
+        private ToolStripMenuItem fiToolStripMenuItem;
+        private ToolStripMenuItem nouToolStripMenuItem;
+        private ToolStripMenuItem deschidereToolStripMenuItem;
+        private ToolStripMenuItem salvareToolStripMenuItem;
+        private ToolStripMenuItem ieșireToolStripMenuItem;
+        private ToolStripMenuItem editareToolStripMenuItem;
+        private ToolStripMenuItem adăugațiSarcinăToolStripMenuItem;
+        private ToolStripMenuItem editațiSarcinaToolStripMenuItem;
+        private ToolStripMenuItem ștergețiSarcinaToolStripMenuItem;
+        private ToolStripMenuItem ajutorToolStripMenuItem;
+        private ToolStripMenuItem despreToolStripMenuItem;
+        private ListBox listBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+        private DateTimePicker dateTimePicker1;
+        private NumericUpDown numericUpDown1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,199 @@
+﻿using System.Security.Policy;
+using System.IO;
+using static Tema5.Form1;
+using System.Runtime.Serialization;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+using System.Xml.Serialization;
+using System.Diagnostics;
+using System.IO.Pipes;
+
+namespace Tema5
+{
+    public partial class Form1 : Form
+    {
+        int contor = 1, contor2 = 1;
+
+        [Serializable]
+        public class Sarcina
+        {
+            public string Nume { get; set; }
+            public string Descriere { get; set; }
+            public DateTime DataScadenta { get; set; }
+            public int Prioritate { get; set; }
+
+            override public string ToString()
+            {
+                return Nume;
+            }
+        }
+
+        List<Sarcina> sarcini = new List<Sarcina>();
+        XmlSerializer serializer = new XmlSerializer(typeof(List<Sarcina>));
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.ReadOnly = true;
+            textBox2.ReadOnly = true;
+            dateTimePicker1.Enabled = false;
+            numericUpDown1.Enabled = false;
+        }
+
+        private void nouToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            textBox1.Clear();
+            textBox2.Clear();
+            dateTimePicker1.Value = DateTime.Now;
+            numericUpDown1.Value = 1;
+
+            contor = 1;
+            contor2 = 1;
+
+            sarcini.Clear();
+
+        }
+       
+        private void deschidereToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "XML files (*.xml)|*.xml";
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (Stream stream = File.OpenRead(openFileDialog.FileName))
+                    {
+                        sarcini = (List<Sarcina>)serializer.Deserialize(stream);
+                        listBox1.Items.Clear();
+                        foreach (Sarcina sarcina in sarcini)
+                        {
+                            listBox1.Items.Add(sarcina);
+                        }
+                    }
+                }
+                catch (SerializationException ex)
+                {
+                    MessageBox.Show("Failed to deserialize. Reason: " + ex.Message);
+                }
+            }
+        }
+
+        private void salvareToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (sarcini.Count > 0)
+            {
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.Filter = "XML File|*.xml";
+                saveFileDialog1.Title = "Salvare ca";
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    using (FileStream fs = new FileStream(saveFileDialog1.FileName, FileMode.Create))
+                    {
+                        serializer.Serialize(fs, sarcini);
+                    }
+                }
+            }
+            else MessageBox.Show("Nu există nicio sarcină de salvat.", "Atenție", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+        private void ieșireToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void adăugațiSarcinăToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add($"Sarcina{contor2}");
+            Sarcina sarcina = new Sarcina();
+
+            int index = listBox1.FindStringExact($"Sarcina{contor2}");
+
+            if (index != ListBox.NoMatches)
+            {
+                listBox1.SelectedIndex = index;
+            }
+            contor2++;
+        }
+
+        private void editațiSarcinaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItems.Count >0)
+            {
+                textBox1.ReadOnly = false;
+                textBox2.ReadOnly = false;
+                dateTimePicker1.Enabled = true;
+                numericUpDown1.Enabled = true;
+            }
+            else MessageBox.Show("Selectați o sarcină.", "Atenție", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void ștergețiSarcinaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+
+        private void despreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("    Această aplicație reprezintă un ToDo list, care îi permite utilizatorului " +
+                "să creeze, editeze și șteargă activități organizate în liste, pe care le poate salva ulterior " +
+                "pe dispozitivul său, pentru a le putea accesa oricând.", "Despre");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            { 
+                Sarcina sarcina = new Sarcina();
+                sarcina.Nume = textBox1.Text;
+                sarcina.Descriere = textBox2.Text;
+                sarcina.DataScadenta = dateTimePicker1.Value;
+                sarcina.Prioritate = (int)numericUpDown1.Value;
+                listBox1.Items.Add(sarcina);
+                sarcini.Add(sarcina);
+
+                textBox1.Clear();
+                textBox2.Clear();
+                dateTimePicker1.Value = DateTime.Now;
+                numericUpDown1.Value = 1;
+
+                listBox1.Items.Remove(listBox1.SelectedItem);
+
+                textBox1.ReadOnly = true;
+                textBox2.ReadOnly = true;
+                dateTimePicker1.Enabled = false;
+                numericUpDown1.Enabled = false;
+            }
+            else MessageBox.Show("Introduceți un nume pentru sarcină.", "Atenție", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            dateTimePicker1.Value = DateTime.Now;
+            numericUpDown1.Value = 1;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Sarcina sarcinas = listBox1.SelectedItem as Sarcina;
+            if (sarcinas != null)
+            {
+                textBox1.Text = sarcinas.Nume;
+                textBox2.Text = sarcinas.Descriere;
+                dateTimePicker1.Value = sarcinas.DataScadenta;
+                numericUpDown1.Value = sarcinas.Prioritate;
+            }
+        }
+
+    }
+}
